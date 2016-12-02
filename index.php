eb63a1bbebcd56d6984259ef6ee79798fb67d314
vkrms <april504@yandex.ru>
2016-12-02 16:31:50 +1000

    separate db cred, debug mysql functions and query

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-$link = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");  //connect check - okay
+    
+    require('init.php'); //доступ к бд в отдельном файле, чтобы не менять постоянно мой логин/пароль и твой
+
 
 if (!$link) {
-    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    echo "Ошибка: Невозможно установить соединение с MySQL.<br>";  //меняем PHP_EOL на <br> потому что мы в html'е
+    echo "Код ошибки errno: " . mysqli_connect_errno() . '<br>';
+    echo "Текст ошибки error: " . mysqli_connect_error() . '<br>';
     exit;
 }
 
-echo "Соединение с MySQL установлено!" . PHP_EOL;
-echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+echo "Соединение с MySQL установлено!<br>";
+echo "Информация о сервере: " . mysqli_get_host_info($link) . '<br>';
 
-$query = "SELECT * FROM road_signs_actual.materials ORDER by ID DESC"; //here goes nothing
+$query = "SELECT * FROM materials ORDER by idmaterials DESC"; //here goes nothing
 
 if ($result = mysqli_query($link, $query)) {
 
@@ -22,7 +24,7 @@ if ($result = mysqli_query($link, $query)) {
 
     /* free result set */
     mysqli_free_result($result);
-}
+} else echo mysqli_error($link);
 
 mysqli_close($link);
 ?>
